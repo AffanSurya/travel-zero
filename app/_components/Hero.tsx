f0007a9934bf907ecea25cfb5b624b357b303179
AffanSurya <affanqwe04@gmail.com>
2025-08-22 20:13:12 +0700

    finished creating authentication using Clerk

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,10 @@
+"use client";
 import HeroVideoDialog from "@/components/magicui/hero-video-dialog";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { useUser } from "@clerk/nextjs";
 import { ArrowDown, Globe2, Icon, Landmark, Plane, Send } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const suggestions = [
@@ -24,6 +27,17 @@ const suggestions = [
 ];
 
 function Hero() {
+    const { user } = useUser();
+    const router = useRouter();
+
+    const onSend = () => {
+        if (!user) {
+            router.push("/sign-in");
+            return;
+        }
+
+        // navigate to Create Trip Planner web page
+    };
     return (
         <div className="mt-24 w-full flex justify-center">
             {/* Content */}
@@ -39,7 +53,7 @@ function Hero() {
                             placeholder="Create a trip for Parise from New york"
                             className="w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none"
                         />
-                        <Button size={"icon"} className="absolute bottom-6 right-6">
+                        <Button size={"icon"} className="absolute bottom-6 right-6" onClick={() => onSend()}>
                             <Send className="h-4 w-4" />
                         </Button>
                     </div>
